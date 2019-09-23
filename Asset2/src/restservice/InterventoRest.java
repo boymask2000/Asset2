c3536fb9f7b7655c6920e5b78151099af31d7794
boymask2000 <boymask2000@yahoo.it>
2019-09-23 12:25:54 +0200

    ,

--- Asset2/src/restservice/InterventoRest.java
+++ Asset2/src/restservice/InterventoRest.java
@@ -2,11 +2,18 @@ package restservice;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.primefaces.json.JSONObject;
+
+import com.google.gson.Gson;
 
 import beans.AssetAlca;
 import beans.ChecklistIntervento;
@@ -89,13 +96,15 @@ public class InterventoRest {
 
 	@POST
 	@Path("/updateIntervento")
+	  @Consumes(MediaType.APPLICATION_JSON)
 	public Response update(InterventoRestBean inter) {
 	
 
 		InterventiDAO dao = new InterventiDAO();
 		dao.update(inter);
 
-		return Response.ok().build();
+		JSONObject object = new JSONObject();
+		return Response.status(200).entity(object.toString()).build();
 	}
 
 	@POST
